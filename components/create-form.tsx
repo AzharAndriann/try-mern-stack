5f7edec8111b5a9bf0740774624da5cd7c2e6b9b
unknown <azhardwiandrian@gmail.com>
2024-11-13 17:33:49 +0700

    schema confirm

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -1,14 +1,30 @@
+"use client"
 import React from 'react'
 import { uploadImage } from '@/lib/action'
+import { useFormState } from 'react-dom'
 
-const CreateForm = () => {
+const CreateForm = () =>
+{
+
+  const [ state, formAction ] = useFormState( uploadImage, null )
+
   return (
-    <form action={uploadImage}>
+    <form action={ formAction }>
       <div className="mb-4 pt-2">
         <input className='py-2 px-4 rounded-sm border border-gray-400 w-full' type="text" name='title' placeholder='Title' />
+        <div aria-live='polite' aria-atomic='true'>
+          <p className='text-sm text-red-500 mt-2'>
+            { state?.error?.title }
+          </p>
+        </div>
       </div>
       <div className="mb-4 pt-2">
         <input className='file:py-2 file:px-4 file:mr-4 file:rounded-sm file:border-0 file:bg-gray-200 hover:file:bg-gray-300 file:cursor-pointer border border-gray-400 w-full' type="file" name='image' placeholder='Title' />
+        <div aria-live='polite' aria-atomic='true'>
+          <p className='text-sm text-red-500 mt-2'>
+            { state?.error?.image }
+          </p>
+        </div>
       </div>
       <div className="mb-4 pt-4">
         <button className='bg-blue-700 text-white w-full font-medium py-2.5 px-6 text-base rounded-sm hover:bg-blue-600' type='submit'>upload</button>
--- lib/action.ts
+++ lib/action.ts
@@ -6,11 +6,11 @@ const UploadSchema = z.object({
   title: z.string().min(1),
   image: z.instanceof(File)
   .refine((file) => file.size > 0, { message: "Image is required" })
-  .refine((file) => file.type.startsWith("image/"), { message: "Only images are allowed" })
+  .refine((file) => file.size === 0 || file.type.startsWith("image/"), { message: "Only images are allowed" })
   .refine((file) => file.size < 4000000, { message: "Image must less than 4mb"})
 })
 
-export const uploadImage = async (formData: FormData) => {
+export const uploadImage = async (prevState: unknown, formData: FormData) => {
   const validatedFields = UploadSchema.safeParse(
     Object.fromEntries(formData.entries())
   )
