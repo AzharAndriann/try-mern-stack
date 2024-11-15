84d69c4713612d5bde6075f952c69d3b08526112
unknown <azhardwiandrian@gmail.com>
2024-11-15 16:59:05 +0700

    add props button

--- components/button.tsx
+++ components/button.tsx
@@ -1,10 +1,21 @@
 "use-client"
+import clsx from "clsx"
 import { useFormState, useFormStatus } from "react-dom"
 
-export const SubmitButton = () =>
+export const SubmitButton = ( { label }: { label: string } ) =>
 {
-  const {pending} = useFormStatus()
+  const { pending } = useFormStatus()
   return (
-    <button className='bg-blue-700 text-white w-full font-medium py-2.5 px-6 text-base rounded-sm hover:bg-blue-600' disabled={pending} type='submit'>upload</button>
+    <button className={ clsx( "bg-blue-700 text-white w-full font-medium py-2.5 px-6 text-base rounded-sm hover:bg-blue-600", { "opacity-50 cursor-progress": pending } ) } disabled={ pending } type='submit'>
+      { label == "upload" ? (
+        <>
+          { pending ? "Uploading..." : "Upload" }
+        </>
+      ) : (
+        <>
+          { pending ? "Updating..." : "Update" }
+        </>
+      ) }
+    </button>
   )
 }
--- components/create-form.tsx
+++ components/create-form.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { uploadImage } from '@/lib/action'
 import { useFormState } from 'react-dom'
+import { SubmitButton } from '@/components/button'
 
 const CreateForm = () =>
 {
@@ -27,6 +28,7 @@ const CreateForm = () =>
         </div>
       </div>
       <div className="mb-4 pt-4">
+        <SubmitButton label="upload" />
       </div>
     </form>
   )
