f441ced4e9ee2681948ae3dd9a07f643cd473f81
unknown <azhardwiandrian@gmail.com>
2024-11-17 20:39:34 +0700

    add card.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <div className="max-w-screen-lg mx-auto py-14">
+      <div className="flex items-end justify-between">
+        <h1 className="text-4xl font-bold">Latest Images</h1>
+        <Link href={"/create"} className="py-3 px-6 bg-blue-600 hover:to-blue-700 text-white">Upload New Image</Link>
+      </div>
+      <div className="grid md:grid-cols-3 gap-5 mt-10">
+        
+      </div>
+    </div>
   );
 }
---
+++ components/card.tsx
@@ -0,0 +1,12 @@
+
+const Card = () => {
+  return (
+    <div className="max-w-sm border border-gray-200 rounded-md shadow">
+      <div className="relative">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Card
