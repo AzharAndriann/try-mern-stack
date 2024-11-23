edcb713b62aca4f94dde1665ec064b0ac4446fbd
unknown <azhardwiandrian@gmail.com>
2024-11-23 22:27:05 +0700

    add data.title

--- components/edit-form.tsx
+++ components/edit-form.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 import { uploadImage } from '@/lib/action'
 import { useFormState } from 'react-dom'
 import { SubmitButton } from '@/components/button'
+import { Upload } from '@prisma/client'
 
-const EditForm = () =>
+const EditForm = ( { data }: { data: Upload } ) =>
 {
 
   const [ state, formAction ] = useFormState( uploadImage, null )
@@ -12,7 +13,7 @@ const EditForm = () =>
   return (
     <form action={ formAction }>
       <div className="mb-4 pt-2">
-        <input className='py-2 px-4 rounded-sm border border-gray-400 w-full' type="text" name='title' placeholder='Title' />
+        <input className='py-2 px-4 rounded-sm border border-gray-400 w-full' type="text" name='title' placeholder='Title' defaultValue={data.title} />
         <div aria-live='polite' aria-atomic='true'>
           <p className='text-sm text-red-500 mt-2'>
             { state?.error?.title }
