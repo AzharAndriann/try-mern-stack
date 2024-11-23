768a3ee75f3fe6e4fe96e4dde95b56c6e1790f76
unknown <azhardwiandrian@gmail.com>
2024-11-23 22:23:02 +0700

    add edit form

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,11 +1,17 @@
-import CreateForm from '@/components/create-form'
+import EditForm from '@/components/edit-form'
+import { getImageById } from '@/lib/data'
+import { notFound } from 'next/navigation'
 
-const EditPage = () => {
+
+const EditPage = async ({params} : {params:{id:string}}) =>
+{
+  const data = await getImageById( params.id )
+  if(!data) return notFound
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-100">
       <div className="bg-white rounded-sm shadow p-8">
-        <h1 className="text-xxl font-bold mb-5">Upload Image</h1>
-        <CreateForm />
+        <h1 className="text-xxl font-bold mb-5">Update Image</h1>
+        <EditForm data={data} />
       </div>
     </div>
   )
